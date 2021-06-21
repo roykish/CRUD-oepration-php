cdee012b11891f21bb093d91a2792a3598d69eab
Kishor Roy <roykishor93@gmail.com>
2021-06-21 14:07:07 +0600

    Developed only form

---
+++ inc/form.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <form action="inc/user.php" method="post" class="mt-5 w-50 mx-auto shadow p-3 mb-5 bg-body rounded" >
+        <h4 classs="display-3 text-center">Login</h4><hr><br>
+        <div class="form-group">
+            <label for="fullname">Fullname</label>
+            <input type="text" class="form-control" id="fullname"  placeholder="Enter full name">
+        </div>
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" placeholder="username">
+        </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  
+</div>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include 'inc/header.php';
+?>
+
+<?php
+include 'inc/form.php';
+?>
+
+<?php
+include 'inc/footer.php';
+?>
